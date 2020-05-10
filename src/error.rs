a7886d1df2aaf0951c73700b189f98473bf70978
Afnan Enayet <afnanenayet@users.noreply.github.com>
2020-05-09 22:47:40 -0400

    Remove failure, use thiserror (#6)
    * Remove all instances of the deprecated `failure` crate
    * Use `thiserror` to derive errors
    * Remove `failure-crate` feature

--- src/error.rs
+++ src/error.rs
@@ -1,45 +1,19 @@
-#[cfg(feature = "failure-crate")]
-use failure::Fail;
+use thiserror::Error;
 
-#[cfg(not(feature = "failure-crate"))]
-use std::fmt::{self, Display};
-
-#[derive(Debug)]
-#[cfg_attr(feature = "failure-crate", derive(Fail))]
-// We allow the name repitition because this struct will not make sense outside of the crate
+/// The different types of errors that can arise from this crate
+#[derive(Debug, Error)]
+// We allow the name repetition because this struct will not make sense outside of the crate
 // otherwise, and this is exported as part of the library.
 #[allow(clippy::module_name_repetitions)]
 pub enum PermutationError {
-    #[cfg_attr(
-        feature = "failure-crate",
-        fail(
-            display = "Attempted to shuffle {}, where the highest number is {}",
-            shuffle, max_shuffle
-        )
-    )]
     /// This error is invoked when the caller attempts to use an index on the `shuffle` method that
     /// is larger than the size of the set.
     ///
     /// The user can only shuffle indices that are within the set, otherwise the hashing algorithm
     /// does not work. `shuffle` is the index that the user called, and `max_shuffle` is the size
     /// of the permutation set (which is also the upper bound for the calling index).
+    #[error("Attempted to shuffle index {shuffle}, but the length of the array is {max_shuffle}")]
     ShuffleOutOfRange { shuffle: u32, max_shuffle: u32 },
-}
-
-#[cfg(not(feature = "failure-crate"))]
-impl Display for PermutationError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            PermutationError::ShuffleOutOfRange {
-                shuffle,
-                max_shuffle: max,
-            } => write!(
-                f,
-                "Attempted to shuffle {}, where the highest number is {}",
-                shuffle, max
-            ),
-        }
-    }
 }
 
 /// A permutation result, which is simply an alias for any type that could return a permutation
